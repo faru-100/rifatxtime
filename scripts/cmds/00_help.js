73b12654da24167473706e086a8e98334b92278a
Rafiyan Rifat <164067230+faru-100@users.noreply.github.com>
2025-01-22 18:09:36 +0600

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🩷|☞Tᴀᴍɪᴍ𖣘𝗔𝗜";
+const doNotDelete = "🩷|☞𝗥𝗜𝗙𝗨𖣘𝗔𝗜";
 /** 
 * @author NTKhang
 * @author: do not delete it
